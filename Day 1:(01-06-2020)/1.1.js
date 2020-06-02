4b7cb56b1f3f6c6ef4629b93e426dbad04faebc5
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-06-02 09:53:12 +0530

    Create 1.1.js

---
+++ Day 1:(01-06-2020)/1.1.js
@@ -0,0 +1,50 @@
+var TechnicalSpecifications = {
+	dimension : {
+		overall length : 3500,
+		overall width : 1600,
+		overall heigth : 1490,
+		wheelbase : 2360,
+		trackwidth : {
+			font : 1505,
+			rear : 1400,
+			},
+		minimum turning radius : 4.5,
+		minimum ground clearance : 170
+		},
+	   capacities : {
+		seatinng capacity : 5,
+		fuel tank capacity : 35,
+		},
+	engine : {
+		type : "KB-series",
+		number of cylinders : 3,
+		number of valves : 12,
+		capacity : 998,
+		Broke x stroke : 73.0 * 79,5,
+		compression ratio : 10.1,
+		maximum power : 67/6200,
+		maximum torque : 90/3500,
+		Fuel distribution : "multi point injection"
+		},
+	transmission:{
+		type : "5-speed MT"
+	} ,
+	chasis : {
+		steering : "rack & pinion, power associated",
+		brakes : {
+			front : "ventilated disc",
+			rear : "drums",
+			}
+		suspensioons : {
+			front : "Macperson struct & coil spring",
+			rear : "isolated trailing link & coil spring",
+			}
+		shock absorber : "gas filled",
+		type : "155/80R13",
+	},
+	weigth : {
+		kerb weigth : 860-880,
+		gross vehicle weigth : 1,320
+	}
+}
+console.log()
