d722e6ac949be7e4a053aad8ff8c3938fddc2f03
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-06-02 07:58:33 +0530

    Create 2.1.js

---
+++ Day 1:(01-06-2020)/2.1.js
@@ -0,0 +1,37 @@
+//2.1 Update the count of Person 2 inside readers Array in Javascript;
+function library{
+
+var library = [
+{
+	title : "javascript",
+	price : 500,
+	readers : [
+		{
+			name : "Person 1",
+			count : 300
+		},
+		{
+			name : "Person 2",
+			count : 400
+		}
+		],
+		authordetails : {
+			name : "Raj",
+			age : 40
+			}
+		},
+		{
+			title : "Nodejs",
+			price : 600,
+			readers : [],
+			authordetails : {
+				name : "Raj",
+				age : 40
+			}
+		}
+	]
+
+library[0].readers[1].count=500;
+console.log(library[0])
+}
+library();
