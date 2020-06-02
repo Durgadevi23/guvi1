2d3a44def1e08cc7a31f72c872478791aec5e690
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-06-02 08:13:30 +0530

    Create 2.4.js

---
+++ Day 1:(01-06-2020)/2.4.js
@@ -0,0 +1,40 @@
+//2.4 Create a New Object in the library for a new Book called SQL.
+
+function newobject(){
+
+var library = [
+{
+	title : "javascript",
+	price : 500,
+	readers : [
+		{
+			name : "Person 1",
+			count : 300
+		},
+		{
+			name : "Person 2",
+			count : 400
+		}
+		],
+		authordetails : {
+			name : "Raj",
+			age : 40
+			}
+		},
+		{
+			title : "Nodejs",
+			price : 600,
+			readers : [],
+			authordetails : {
+				name : "Raj",
+				age : 40
+			}
+		}
+	]
+
+library[2].newbook={
+New book : "SQL"
+}
+console.log(library[1])
+}
+newobject();
