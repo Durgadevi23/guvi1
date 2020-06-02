1b151018c01016e5d8720899f337bda54b1dca81
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-06-02 17:57:49 +0530

    Create 1.js

---
+++ wfh_task/Day 2:(02-06-2020)/1.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<script>
+var request = new XMLHttpRequest()
+//Http request for getting API
+request.open('GET','https://restcountries.eu/rest/v2/all',true)
+request.onload = function(){
+//converting data inform of json
+var jsonData = JSON.parse(this.response)
+for(var i in jsonData)
+{
+  //displaying country name and flag url
+  console.log("country name:",jsonData[i].name,"flag:" ,jsonData[i].flag)
+}
+}
+request.send()
+
+</script>
+</body>
+</html>
