ea77ce5c53dea0b782d478bf41bb53c214365498
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-06-04 19:41:02 +0530

    Update 4.js

--- wfh_task/Day 3:(03-06-2020)/4.js
+++ wfh_task/Day 3:(03-06-2020)/4.js
@@ -30,31 +30,26 @@ console.log(sum)
 //4.4. Return all the prime numbers in an array
 
 var array = [1,2,3,4,5,6]
-var prime = array.filter((t)=>
- if(t>1)
- var inc=0
+var prime = array.filter((t)=>{
  for(i=2;i<t;i++){
  if(t%i == 0)
  {
- inc = 1
+ return false;
  }
+ return t !==1;
  }
- 
- if(inc==0)
- {
-  t
- }
- )
+});
  console.log(prime)
+ 
  
 //4.5. Return all the palindromes in an array
 
 var array = ['sas','task','madam']
-var palindrome = array.filter((t)=>
+var palindrome = array.filter((t)=>{
   array2=t.split('').reverse().join('')
   if(t == array2)
   {
-   t
+   return t
   }
-  );
+  });
   console.log(palindrome)
