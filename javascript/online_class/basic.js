81c95e2b491bcb92b435238b49fa9989c3fa2e03
Durgadevi23 <60968388+Durgadevi23@users.noreply.github.com>
2020-06-08 10:49:59 +0530

    Create basic.js

---
+++ javascript/online_class/basic.js
@@ -0,0 +1,21 @@
+function demoFun(varA,varB){
+if(varA==100){
+return varA-varB;
+
+}
+console.log(varA+varB) //
+return varA+varB
+console.log(varA+varB) //
+
+}
+
+varA1 = demoFun(100,200)
+console.log(varA1)//
+
+var object = [
+
+function demoFun(a,b){
+console.log(a+b)
+}
+]
+demoFun(1,2)
